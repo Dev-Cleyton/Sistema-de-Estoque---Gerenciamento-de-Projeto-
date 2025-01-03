3de89f630748ec294afe7d4f9cb8698a03c0c43a
cleyton <cleyton@Claytow>
2025-01-03 19:21:11 -0300

    Sistema de Estoque Farmacia -> Este projeto é voltado para pequenas e médias empresas, oferecendo uma solução eficiente para a gestão de vendas e controle de estoque. O sistema é ideal para empresas comerciais que buscam otimizar processos e aumentar a produtividade.

--- src/br/com/sistema/jdbc/TestarConexao.java
+++ src/br/com/sistema/jdbc/TestarConexao.java
@@ -18,7 +18,8 @@ public class TestarConexao {
      */
     public static void main(String[] args) {
         try {
-            new ConexaoBanco().pegarConexao();
+            //new ConexaoBanco().pegarConexao();
+            new ConexaoBancoRelatorios().conecta();
             JOptionPane.showMessageDialog(null,"Conectado com sucesso ao banco de dados!");
         } catch (HeadlessException erro) {
             JOptionPane.showMessageDialog(null,"Erro ao tenatr conectado com o banco de dados" + erro.getMessage());
